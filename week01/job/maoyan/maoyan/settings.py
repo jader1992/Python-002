73db8037262009ea2b49d04c4ad92293e37525c5
peiyu <1249200310@qq.com>
2020-08-01 00:10:56 +0800

    full week job

---
+++ week01/job/maoyan/maoyan/settings.py
@@ -0,0 +1,89 @@
+# Scrapy settings for maoyan project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'maoyan'
+
+SPIDER_MODULES = ['maoyan.spiders']
+NEWSPIDER_MODULE = 'maoyan.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'maoyan (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 1
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/84.0.4147.89 Safari/537.36',
+    'Cookie': '__mta=46947736.1595772898549.1596004106629.1596163789775.8; uuid_n_v=v1; uuid=623213C0CF4A11EA9D9027CAB3F025F8A1814158FA1742E5B48F549EDC7F0900; _csrf=a83958c5fe77e1aa5569d63f33e8fab6503fb91797c2c915f11e62b73e5593e2; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1595772898; _lxsdk_cuid=1738b7a0c734d-0e015a05a9adb7-31617402-1fa400-1738b7a0c7441; _lxsdk=623213C0CF4A11EA9D9027CAB3F025F8A1814158FA1742E5B48F549EDC7F0900; mojo-uuid=dfd55e56875b91c9105e6b7438ad37ea; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1596206370; __mta=46947736.1595772898549.1596163789775.1596206370570.9; _lxsdk_s=173a56d5c3b-40-6f2-88f%7C%7C1'
+}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'maoyan.middlewares.MaoyanSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+# DOWNLOADER_MIDDLEWARES = {
+#    'maoyan.middlewares.MaoyanDownloaderMiddleware': 543,
+# }
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   'maoyan.pipelines.MaoyanPipeline': 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
--- week01/job/week01_bs4.py
+++ week01/job/week01_bs4.py
@@ -2,82 +2,56 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas
-import numpy
 import re
-import time
-
 
 movieUrl = 'https://maoyan.com/films?showType=3'
 userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
-cookie = 'uuid_n_v=v1; uuid=623213C0CF4A11EA9D9027CAB3F025F8A1814158FA1742E5B48F549EDC7F0900; _csrf=a83958c5fe77e1aa5569d63f33e8fab6503fb91797c2c915f11e62b73e5593e2; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1595772898; _lxsdk_cuid=1738b7a0c734d-0e015a05a9adb7-31617402-1fa400-1738b7a0c7441; _lxsdk=623213C0CF4A11EA9D9027CAB3F025F8A1814158FA1742E5B48F549EDC7F0900; mojo-uuid=dfd55e56875b91c9105e6b7438ad37ea; mojo-session-id={"id":"daa4a2016fc47815eef63c125b1c3aaf","time":1595772898493}; mojo-trace-id=2; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1595772905; __mta=46947736.1595772898549.1595772898549.1595772905206.2; _lxsdk_s=1738b7a0c77-abf-dc1-7aa%7C%7C4'
-
-fetchMovieTitle = []
-fetchMovieUrl = []
-fetchMovieDate = []
-fetchMovieType = []
+cookie = '__mta=46947736.1595772898549.1596004106629.1596163789775.8; uuid_n_v=v1; uuid=623213C0CF4A11EA9D9027CAB3F025F8A1814158FA1742E5B48F549EDC7F0900; _csrf=a83958c5fe77e1aa5569d63f33e8fab6503fb91797c2c915f11e62b73e5593e2; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1595772898; _lxsdk_cuid=1738b7a0c734d-0e015a05a9adb7-31617402-1fa400-1738b7a0c7441; _lxsdk=623213C0CF4A11EA9D9027CAB3F025F8A1814158FA1742E5B48F549EDC7F0900; mojo-uuid=dfd55e56875b91c9105e6b7438ad37ea; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1596206370; __mta=46947736.1595772898549.1596163789775.1596206370570.9; _lxsdk_s=173a56d5c3b-40-6f2-88f%7C%7C1'
+movieList = []
 
 def getList(myurl, cookie, userAgent):
 
-    baseUrl = 'https://maoyan.com'
     header = {
         "cookie": cookie,
         "User-Agent": userAgent
     }
-
-
     response = requests.get(myurl, headers=header)
     bs_info = bs(response.text, 'html.parser')
 
-    data = []
-    tags = bs_info.find_all('div', attrs={'class': 'channel-detail movie-item-title'})
+    tags = bs_info.find_all('div', attrs={'class': 'movie-hover-info'})
+    if len(tags) == 0:
+        return movieList
 
-    for i in range(0,10):
-        atag = tags[i]
-        film = atag.text
-        href = atag.find('a').get('href')
-        if href is None:
-            continue
+    i , movieData = 0, []
+    for tag in tags:
+        if i > 9:
+            break
         else :
-            href = baseUrl + href
-        fetchMovieTitle.append(film.strip())
-        fetchMovieUrl.append(href)
-        # movieDetail = getDetail(href, userAgent, detailCookie)
-        # data.append(film.strip())
-        # data.append(movieDetail[0].strip())
-        # data.append(movieDetail[1].strip())
-    # output(data)
+            i += 1
+            movieName = tag.find('span', attrs={'class': 'name'}).get_text()
+            scoreInt = tag.find('i', attrs={'class': "integer"})
+            if scoreInt is None:
+                scoreInt = ''
+            else:
+                scoreInt = scoreInt.get_text()
+            scoreFraction = tag.find('i', attrs={'class': 'fraction'})
+            if scoreFraction is None:
+                scoreFraction = ''
+            else :
+                scoreFraction = scoreFraction.get_text()
+            movieScore = scoreInt + scoreFraction
+            movieType = re.sub('类型:', '', tag.find_all('div', attrs={'class': 'movie-hover-title'})[1].text).strip()
+            movieActor = re.sub('主演:', '', tag.find_all('div', attrs={'class': 'movie-hover-title'})[2].text).strip()
+            movieTime = re.sub('上映时间:', '', tag.find_all('div', attrs={'class': 'movie-hover-title'})[3].text).strip()
+            movieData.append([i, movieName, movieType, movieActor, movieScore, movieTime])
 
-def getDetail(url, cookie, userAgent):
+    return movieData
 
-    header = {
-        "cookie": cookie,
-        "User-Agent": userAgent
-    }
-    response = requests.get(url, headers=header)
-    bs_info = bs(response.text, 'html.parser')
-    movieTypes = bs_info.find_all('a', attrs={'class': 'text-link'})
-    ellipsis = list(bs_info.find_all('li', attrs={'class': 'ellipsis'}))
-    showTime = ellipsis[-1].text
-    type = []
-    for movie in movieTypes:
-        type.append(movie.text.strip())
-    fetchMovieDate.append(showTime.strip())
-    fetchMovieType.append(','.join(type))
-
-
-def output(data):
-    movieData = pandas.DataFrame(data=data)
-    movieData.to_csv('./movie.csv', encoding='utf-8', index=False, header=False, sep=' ')
-
-#抓取电影名称，电影链接
-getList(movieUrl, userAgent, cookie)
-time.sleep(2)
-
-# 获取电影类型，上映时间
-detailCookie = '__mta=46947736.1595772898549.1595773510801.1595775004065.5; uuid_n_v=v1; uuid=623213C0CF4A11EA9D9027CAB3F025F8A1814158FA1742E5B48F549EDC7F0900; _csrf=a83958c5fe77e1aa5569d63f33e8fab6503fb91797c2c915f11e62b73e5593e2; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1595772898; _lxsdk_cuid=1738b7a0c734d-0e015a05a9adb7-31617402-1fa400-1738b7a0c7441; _lxsdk=623213C0CF4A11EA9D9027CAB3F025F8A1814158FA1742E5B48F549EDC7F0900; mojo-uuid=dfd55e56875b91c9105e6b7438ad37ea; __mta=46947736.1595772898549.1595772905206.1595772915865.3; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1595775004; _lxsdk_s=1738c397732-293-494-a97%7C%7C1'
-for url in fetchMovieUrl:
-    getDetail(url, detailCookie, userAgent)
-
-data = [fetchMovieDate,fetchMovieType,fetchMovieTitle]
-data = (numpy.rot90(data,-1)).tolist()
-output(data)
+if __name__ == "__main__":
+    movieData = getList(movieUrl, cookie, userAgent)
+    if len(movieData) == 0:
+        print("There is no data need output")
+    else :
+        movieData = pandas.DataFrame(data=movieData)
+        movieData.to_csv('./movie.csv', encoding='utf-8',  header=['序号', '电影名称', '电影类型', '电影主演', '电影分数', '上映时间'], index=False, sep=',')
+        print("data is output")
